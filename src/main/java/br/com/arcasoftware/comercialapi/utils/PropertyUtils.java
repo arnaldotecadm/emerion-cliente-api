ab73c2f23ba9eb22fa0279d5636ef74d984d0206
Arnaldo Cicero Bezerra <arnaldotecadm@hotmail.com>
2022-02-17 01:31:23 +0000

    FIrst Commit

--- src/main/java/br/com/arcasoftware/comercialapi/utils/PropertyUtils.java
+++ src/main/java/br/com/arcasoftware/comercialapi/utils/PropertyUtils.java
@@ -1,5 +1,8 @@
 package br.com.arcasoftware.comercialapi.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,4 +26,9 @@ public class PropertyUtils {
     @Value("${spring.liquibase.enabled}")
     private boolean shouldRunLiquibase;
 
+    public static void main(String[] args) {
+        String json = "{\"cnpjEmitente\":\"null\",\"fileName\":\"35160107022495000107550010000666471979472972-nfe.xml\"}";
+        JsonObject convertedObject = new Gson().fromJson(json, JsonObject.class);
+        System.out.println(convertedObject);
+    }
 }
