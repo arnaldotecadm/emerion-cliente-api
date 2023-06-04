50feac97d3341662dfc6fbc130ab6fdc37ad1bc3
Arnaldo Cicero Bezerra <arnaldotecadm@hotmail.com>
2023-06-04 21:20:04 +0100

    Update entries in case they are already present in the database

--- src/main/java/br/com/arcasoftware/comercialapi/application/repository/CustomerOrderDetailRepository.java
+++ src/main/java/br/com/arcasoftware/comercialapi/application/repository/CustomerOrderDetailRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerOrderDetailRepository extends JpaRepository<CustomerOrderDetail, UUID> {
 
     List<CustomerOrderDetail> findByCnpjEmpresaAndCodcli(String cnpjEmpresa, String codcli);
+    List<CustomerOrderDetail> findByCnpjEmpresaAndNumres(String cnpjEmpresa, String numres);
 
     List<CustomerOrderDetail> findByCustomerOrder(UUID customerOrder);
 }
